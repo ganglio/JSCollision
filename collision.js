7fb2dc06e1411e45f381b6a41d57ddfddd8134a8
Roberto Torella <robertot@square-enix.com>
2012-11-29 15:28:12 +0000

    Added a bit of inline documentation to be polite

--- collision.js
+++ collision.js
@@ -1,8 +1,20 @@
 ;(function(window,document,undefined){
 
+	/**
+	 * [collision description]
+	 * @param  {String}      selector   A selector describing the objects to check for collisions
+	 * @param  {HTMLElement} container  An optional container to quicken the search
+	 * @return {object}                 self (handy for chaining)
+	 */
 	var self = window.collision = function(selector,container) {
 		self.elements = $$(selector,container);
 
+		/**
+		 * Test if the two objects collide
+		 * @param  {object}  o0  The first object
+		 * @param  {object}  o1  The second object
+		 * @return {boolean}     True if the objects collide
+		 */
 		window.collision.test_collision = function(o0,o1) {
 			if (o0.shape=="box" && o1.shape=="box") {
 				test = !( o0.p1.y < o1.p0.y || o0.p0.y > o1.p1.y || o0.p1.x < o1.p0.x || o0.p0.x > o1.p1.x )
@@ -21,10 +33,24 @@
 			return test;
 		}
 
+		/**
+		 * Helper function to check if a point is inside a circle
+		 * @param  {object{x:,y:}}  c  The circle center
+		 * @param  {number}         r  The circle radius
+		 * @param  {object{x:,y:}}  p  The point to check
+		 * @return {boolean}           True if the point is inside the circle
+		 */
 		function inCircle(c,r,p) {
 			return Math.sqrt((c.x-p.x)*(c.x-p.x)+(c.y-p.y)*(c.y-p.y))<r;
 		}
 
+		/**
+		 * Helper function to check if a point is inside a box
+		 * @param  {object{x:,y:}}  p1  Upper left corner of the box
+		 * @param  {object{x:,y:}}  p2  Lowe right corner of the box
+		 * @param  {object{x:,y:}}  p3  The point to check
+		 * @return {boolean}            True if the point is inside the circle
+		 */
 		function inBox(p1,p2,p3) {
 			return p3.x>p1.x && p3.x<p2.x && p3.y>p1.y && p3.y<p2.y;
 		}
@@ -51,6 +77,10 @@
 		return self;
 	}
 
+	/**
+	 * Checks if there is any collision among the objects selected
+	 * @return {Array}  A list of colliding objects
+	 */
 	window.collision.colliding = function() {
 		var colliding = [];
 		self.elements.forEach(function(current_element){
@@ -68,6 +98,12 @@
 		return colliding;
 	}
 
+	/**
+	 * Simple and dirty query selector alla Sizzle (but simpler)
+	 * @param  {String}       expr  The actual selector
+	 * @param  {HTMLElement}  con   An optional container to quicken the selection
+	 * @return {Array}              An array of selected elements
+	 */
 	function $$(expr, con) {
 		return [].slice.call((con || document).querySelectorAll(expr));
 	}
